fc648a67e98fe72f2f4e8211882acccc421744bb
unknown <anzzhhela98@gmail.com>
2020-12-10 15:56:20 +0200

    practice

---
+++ Final-Exam-Preparation-Fundamentals/01.Dictionary/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new SortedDictionary<string, List<string>>();
+
+            string[] input = Console
+                          .ReadLine()
+                          .Split(new string[] { " | " },
+                           StringSplitOptions.RemoveEmptyEntries)
+                          .ToArray();
+
+            foreach (var wrd in input)
+            {
+                string[] splitted = wrd
+                    .Split(new string[] { ": " },
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 1; i < splitted.Length; i++)
+                {
+                    if (!dictionary.ContainsKey(splitted[0]))
+                    {
+                        dictionary.Add(splitted[0], new List<string>());
+
+                    }
+                    dictionary[splitted[0]].Add($" -{splitted[i]}");
+                }
+            }
+            var word = Console.ReadLine().Split(" | ");
+
+            foreach (var newWrd in word)
+            {
+                if (dictionary.ContainsKey(newWrd))
+                {
+                    foreach (var item in dictionary.Where(x => x.Key == newWrd))
+                    {
+                        Console.WriteLine(newWrd);
+                        Console.WriteLine(string.Join("\n", item.Value
+                            .OrderByDescending(x => x.Length)));
+                    }
+                }
+            }
+            string command = Console.ReadLine();
+            if (command == "List")
+            {
+                Console.WriteLine(string.Join(" ", dictionary
+                    .Select(x => x.Key)));
+            }
+        }
+    }
+}
---
+++ Final-Exam-Preparation-Fundamentals/02. Deciphering/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace _02._Deciphering
+{
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var replaceString = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Regex regex = new Regex(@"[d-z#|{}]+");
+            string result = "";
+            var match = regex.Match(input);
+
+            if (input.Length != match.Length)
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+            else
+            {
+                for (int i = 0; i < match.Length; i++)
+                {
+                    result += ((char)(input[i] - 3));
+                }
+            }
+
+            while (result.Contains(replaceString[0]))
+            {
+                result = result.Replace(replaceString[0], replaceString[1]);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
